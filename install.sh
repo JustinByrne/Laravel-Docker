d06e8784941032f4393fc76ce785eec2f7e0b9ec
Justin Byrne <theskaterjesus@gmail.com>
2020-12-21 15:40:38 +0000

    Added Self-signed SSL Cert

--- install.sh
+++ install.sh
@@ -3,43 +3,46 @@
 # empty the shell
 clear
 
-# Project name
+# Getting user input
 echo -n Project name: 
 read app
-
-sed  -i "s/container_name: /container_name: $app/g" docker-compose.yml
-sed  -i 's/app-network/'"$app"'network/g' docker-compose.yml
-sed  -i 's/dbdata/'"$app"'dbdata/g' docker-compose.yml
-
-
-# MySQL root password
+echo -n Domain name:
+read domain
 echo -n MySQL root password: 
 read -s sqlRoot
-echo
-
-sed  -i "s/your_mysql_root_password/$sqlRoot/g" docker-compose.yml
-
-
-# MySQL laraveluser & password
+echo 
 echo -n Laravel MySQL username: 
 read sqlUser
 echo -n Laravel MySQL password: 
 read -s sqlPass
-echo
+echo 
 
+# Updating the docker-compose file
+sed  -i "s/container_name: /container_name: $app/g" docker-compose.yml
+sed  -i 's/app-network/'"$app"'network/g' docker-compose.yml
+sed  -i 's/dbdata/'"$app"'dbdata/g' docker-compose.yml
+sed  -i "s/your_mysql_root_password/$sqlRoot/g" docker-compose.yml
 sed  -i "s/MYSQL_USER:/MYSQL_USER: $sqlUser/g" docker-compose.yml
 sed  -i "s/MYSQL_PASSWORD:/MYSQL_PASSWORD: $sqlPass/g" docker-compose.yml
-cd ./app
-cp .env.example .env
-sed  -i "s/DB_USERNAME=root/DB_USERNAME=$sqlUser/g" .env
-sed  -i "s/DB_PASSWORD=/DB_PASSWORD=$sqlPass/g" .env
 
+# Making changes to .env file
+cp $(pwd)/app/.env.example $(pwd)/app/.env
+sed  -i "s/DB_USERNAME=root/DB_USERNAME=$sqlUser/g" $(pwd)/app/.env
+sed  -i "s/DB_PASSWORD=/DB_PASSWORD=$sqlPass/g" $(pwd)/app/.env
+sed  -i 's/DB_HOST=127.0.0.1/DB_HOST='"$app"'db/g' $(pwd)/app/.env
+
+# Making changes to the nginx config
+sed  -i "s/server_name localhost/server_name $domain/g" $(pwd)/nginx/conf.d/app.conf
 
 # laravel setup
-sed  -i 's/DB_HOST=127.0.0.1/DB_HOST='"$app"'db/g' .env
-docker run --rm -v $(pwd):/app composer install
-sudo chown -R $USER:$USER ./
-cd ../
+docker run --rm -v $(pwd)/app/:/app composer install
+sudo chown -R $USER:$USER $(pwd)/app/
+sudo chown -R $USER:$USER $(pwd)/nginx/ssl/
+
+# Creating the certificate
+docker run --rm -e PUBLIC_CN=$domain -v $(pwd)/nginx/ssl/:/etc/ssl/certs pgarrett/openssl-alpine
+
+# Starting docker and finsihing laravel install
 docker-compose up -d
 docker-compose exec app php artisan key:generate
 docker-compose exec app php artisan config:cache
