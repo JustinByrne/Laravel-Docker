7afeb305e60624b417d341880b939776fb590054
Justin Byrne <theskaterjesus@gmail.com>
2020-12-21 16:21:09 +0000

    Fixed incorrect replace settings

--- install.ps1
+++ install.ps1
@@ -2,13 +2,10 @@
 cls
 
 # Configuration files
-$compose = "docker-compose.yml"
-$composeContent = Get-Content -Path $compose
+$compose = ".\docker-compose.yml"
 $config = ".\nginx\conf.d\app.conf"
-$configContent = Get-Content -Path $config
 $envExample = ".\app\.env.example"
 $env = ".\app\.env"
-$envContent = Get-Content -Path $env
 
 # Getting user Input
 $app = Read-Host "Project name"
@@ -18,21 +15,24 @@ $sqlUser = Read-Host "Laravel MySQL Username"
 $sqlPass = Read-Host "Laravel MySQL Password"
 
 # Updating the compose file with details above
-$composeContent -replace 'container_name: ', "container_name: $($app)" | Set-Content -Path $compose | Out-Null
-$composeContent -replace 'app-network', "$($app)network" | Set-Content -Path $compose | Out-Null
-$composeContent -replace 'dbdata', "$($app)dbdata" | Set-Content -Path $compose | Out-Null
-$composeContent -replace 'your_mysql_root_password', "$($sqlRoot)" | Set-Content -Path $compose | Out-Null
-$composeContent -replace 'MYSQL_USER:', "MYSQL_USER: $($sqlUser)" | Set-Content -Path $compose | Out-Null
-$composeContent -replace 'MYSQL_PASSWORD:', "MYSQL_PASSWORD: $($sqlPass)" | Set-Content -Path $compose | Out-Null
+$composeContent = Get-Content -Path $compose
+$composeContent -replace 'container_name: ', "container_name: $($app)" `
+    -replace 'app-network', "$($app)network" `
+    -replace 'dbdata', "$($app)dbdata" `
+    -replace 'your_mysql_root_password', "$($sqlRoot)" `
+    -replace 'MYSQL_USER:', "MYSQL_USER: $($sqlUser)" `
+    -replace 'MYSQL_PASSWORD:', "MYSQL_PASSWORD: $($sqlPass)" | Set-Content -Path $compose | Out-Null
 
 # Updating nginx configuration
+$configContent = Get-Content -Path $config
 $configContent -replace 'server_name localhost', "server_name $($domain)" | Set-Content -Path $config | Out-Null
 
 # Updating settings in the .env file
 Copy-Item -Path $envExample -Destination $env
-$envContent -replace 'DB_USERNAME=root', "DB_USERNAME=$($sqlUser)" | Set-Content -Path $env | Out-Null
-$envContent -replace 'DB_PASSWORD=', "DB_PASSWORD=$($sqlPass)" | Set-Content -Path $env | Out-Null
-$envContent -replace 'DB_HOST=127.0.0.1', "DB_HOST=$($app)db" | Set-Content -Path $env | Out-Null
+$envContent = Get-Content -Path $env
+$envContent -replace 'DB_USERNAME=root', "DB_USERNAME=$($sqlUser)" `
+    -replace 'DB_PASSWORD=', "DB_PASSWORD=$($sqlPass)" `
+    -replace 'DB_HOST=127.0.0.1', "DB_HOST=$($app)db" | Set-Content -Path $env | Out-Null
 
 # Installing with composer
 docker run --rm -v $pwd/app/:/app composer install
